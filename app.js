9418d0bbd1773063de59a930bc0cd605c6e912ca
Vasil Bit <vasil.bituni@gmail.com>
2021-03-09 11:41:01 +0100

    Add make players choice

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// Gameboard
+
 const gameboard = {
     gameField: ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'O', 'X'],
 }
+
+
+//Player factory
 
 const player = (name, sign) => {
     
@@ -11,18 +16,42 @@ const player = (name, sign) => {
 };
 
 
-function renderChoices () {
-const fieldElements = document.querySelectorAll('.gameboard-field');
-let i = 0;
+// Update choices on the screen
 
-fieldElements.forEach(x => {
-    x.textContent = gameboard.gameField[i]
-    i++
-}); 
+
+function renderChoices () {
+    const fieldElements = document.querySelectorAll('.gameboard-field');
+
+
+    let i = 0;
+
+    fieldElements.forEach(x => {
+        x.textContent = gameboard.gameField[i]
+        i++
+    }); 
+}
+
+renderChoices();
+
+
+// Add Marks on field
+
+const boardContainerElement = document.getElementById('gameboard-container')
+
+function addMark (e) {
+    if (e.target != e.currentTarget) {
+        let position = e.target.id;
+        console.log(position)
+        gameboard.gameField[position] = 'X';
+    }
+    renderChoices();
 
 }
 
-renderChoices()
+boardContainerElement.addEventListener('click', addMark);
+
+
+
 
 
 
