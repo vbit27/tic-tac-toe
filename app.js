ec85575ba5faa419a614656330ea91a0356976fb
Vasil Bit <vasil.bituni@gmail.com>
2021-03-13 09:43:20 +0100

    Add boardGame function

--- app.js
+++ app.js
@@ -1,119 +1,34 @@
+"use strict";
+
+const Player = (sign) => {
+    this.sign = sign;
+
+    getSign() = () => {
+        return sign;
+    }
+
+    return { getSign }
+}
 
 
-let startGame = (() => {
+const boardGame = (() => {
 
-    const addNameElement = document.getElementById('getInput');
-
-        function chooseName() {
-        const  typedNameOne = document.getElementById('player-1-name').value;
-        const  typedNameTwo = document.getElementById('player-2-name').value;
-        const container = document.querySelector('.container');
-            
-            player1 = {
-                name: typedNameOne,
-                sign: 'X'
-            }
-
-            player2 = {
-                name: typedNameTwo,
-                sign: 'O'
-            }
-
-            currentPlayer = player1;
-
-            container.classList.toggle('visible');
-        }
-
-    addNameElement.addEventListener('click', chooseName);
-
-})();
-
-
-let player1;
-let player2;
-
-let currentPlayer
-
-
-
-let game = (() => { 
-
-    gameState = ['', '', '', '', '', '', '', '', ''];
-
+    let board = ["", "", "", "", "", "", "", "", ""];
  
-    const cellElement = document.querySelectorAll('.cell');
-    const gameStatus = document.getElementById('game--status')
-    const restartElement = document.getElementById('game--restart')
+    const setField = (index, sign) => {
+        board[index] = sign;
+    };
 
+    const getIndex = (index) => {
+        return board[index];
+    };
 
-    function handleCellPlayed(e) {
-        let index = e.target.id
-        if (!gameState[index]) {
-            gameState[index] = currentPlayer.sign
-            e.target.innerHTML = currentPlayer.sign;
-            handleResultValidation();
-        }    
-    }
-
-
-    function handleResultValidation() {
-        if (
-            gameState[0] == gameState[1] && gameState[1] == gameState[2] && (gameState[2] == 'X' || gameState[2] == 'O') ||
-            gameState[3] == gameState[4] && gameState[4] == gameState[5] && (gameState[5] == 'X' || gameState[5] == 'O') ||
-            gameState[6] == gameState[7] && gameState[7] == gameState[8] && (gameState[8] == 'X' || gameState[8] == 'O') ||
-            gameState[0] == gameState[3] && gameState[3] == gameState[6] && (gameState[6] == 'X' || gameState[6] == 'O') ||
-            gameState[1] == gameState[4] && gameState[4] == gameState[7] && (gameState[7] == 'X' || gameState[7] == 'O') ||
-            gameState[2] == gameState[5] && gameState[5] == gameState[8] && (gameState[8] == 'X' || gameState[8] == 'O') ||
-            gameState[0] == gameState[4] && gameState[4] == gameState[8] && (gameState[8] == 'X' || gameState[8] == 'O') ||
-            gameState[2] == gameState[4] && gameState[4] == gameState[6] && (gameState[6] == 'X' || gameState[6] == 'O')) {
-                alert(`Winner is ${currentPlayer.name}`)
-            }  else if (!gameState.includes('')) {
-                alert("It's a draw");
-            } else handlePlayerChange()
-            updateGameStatus();
-    }
-
-  
-
-
-    function handlePlayerChange() {
-        if (currentPlayer == player1) {
-            currentPlayer = player2;
-        } else {
-            currentPlayer = player1;
+    const reset = () => {
+        for (let i=0; i < board.length; i++) {
+            board[i] = '';
         }
-    }
+    };
 
-
-    function handleRestartGame() {
-        gameState = ['', '', '', '', '', '', '', '', ''];
-        currentPlayer = player1;
-        cellElement.forEach(x => x.innerHTML = '');
-        gameStatus.innerHTML = `${currentPlayer.name}'s turn`;
-    }
-
-
-    function updateGameStatus() {
-        gameStatus.innerHTML = `${currentPlayer.name}'s turn`;
-    }
-
-    cellElement.forEach(x => x.addEventListener('click', function(e){
-        handleCellPlayed(e);
-    }))
-
-    restartElement.addEventListener('click', handleRestartGame);
+    return { setField, getIndex, reset}
 
 })();
-
-
-
-  /*
-    [0, 1, 2]
-    [3, 4, 5]
-    [6, 7, 8]
-    [0, 3, 6]
-    [1, 4, 7]
-    [2, 5, 8]
-    [0, 4, 8]
-    [2, 4, 6]
-    */
